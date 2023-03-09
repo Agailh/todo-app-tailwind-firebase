bb5b0d992e1596d886f6fa477f36680869b39dc0
Agailh <agailham86@gmail.com>
2023-03-09 14:40:19 +0700

    CRUD function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { AiOutlinePlus } from "react-icons/ai";
 import Todo from "./Todo";
 import { db } from "./firebase";
 
-import { onSnapshot, query, collection } from "firebase/firestore";
+import { onSnapshot, query, collection, updateDoc, doc, addDoc, deleteDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
@@ -16,8 +17,21 @@ const style = {
 };
 function App() {
   const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
 
   //create todo
+  const createTodo = async (e) => {
+    e.preventDefault(e);
+    if (input === "") {
+      alert("Please enter a valid todo");
+      return;
+    }
+    await addDoc(collection(db, "todos"), {
+      text: input,
+      completed: false,
+    });
+  };
+
   //read todo from firebase
   useEffect(() => {
     const q = query(collection(db, "todos"));
@@ -32,24 +46,31 @@ function App() {
   }, []);
 
   //update
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+
   //delete in firebase
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
 
   return (
     <div className={style.bg}>
       <div className={style.container}>
         <h3 className={style.heading}>Todo App</h3>
-        <form className={style.form}>
-          <input type=" text" placeholder="add todo" className={style.input} />
+        <form onSubmit={createTodo} className={style.form}>
+          <input value={input} onChange={(e) => setInput(e.target.value)} type=" text" placeholder="add todo" className={style.input} />
           <button className={style.button}>
             <AiOutlinePlus size={30}></AiOutlinePlus>
           </button>
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} />
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
           ))}
         </ul>
-        <p className={style.count}>U have 2 todos</p>
+        {todos.length < 1 ? null : <p className={style.count}>{`U have ${todos.length} todos !!!`}</p>}
       </div>
     </div>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -10,14 +10,16 @@ const style = {
   button: `cursor-pointer flex items-center`,
 };
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
   return (
     <li className={todo.completed ? style.liComplete : style.li}>
       <div className={style.row}>
-        <input type="checkbox" checked={todo.completed ? "checked" : ""} />
-        <p className={todo.completed ? style.textComplete : style.text}>{todo.text}</p>
+        <input onChange={() => toggleComplete(todo)} type="checkbox" checked={todo.completed ? "checked" : ""} />
+        <p onClick={() => toggleComplete(todo)} className={todo.completed ? style.textComplete : style.text}>
+          {todo.text}
+        </p>
       </div>
-      <button>{<FaRegTrashAlt></FaRegTrashAlt>}</button>
+      <button onClick={() => deleteTodo(todo.id)}>{<FaRegTrashAlt></FaRegTrashAlt>}</button>
     </li>
   );
 };
